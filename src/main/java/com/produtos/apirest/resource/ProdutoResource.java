d33e002914ed8f157586edaa791850e5a6ba51d9
Frederico Thomaz <fredericoufsj@yahoo.com.br>
2022-03-05 22:39:47 -0300

    método Post

--- src/main/java/com/produtos/apirest/resource/ProdutoResource.java
+++ src/main/java/com/produtos/apirest/resource/ProdutoResource.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,15 +18,21 @@ import com.produtos.apirest.repository.ProdutoRepository;
 public class ProdutoResource {
 
 	@Autowired
-	ProdutoRepository produtoRepositoy;
+	ProdutoRepository produtoRepository;
 	
 	@GetMapping("/produtos")
 	public List<Produto> listaProdutos(){
-		return produtoRepositoy.findAll();
+		return produtoRepository.findAll();
 	}
 	
 	@GetMapping("/produto/{id}")
 	public Produto listaProdutoUnico(@PathVariable(value="id")long id){
-		return produtoRepositoy.findById(id);
+		return produtoRepository.findById(id);
 	}
+	
+	@PostMapping("/produto")
+	public Produto salvaProduto(@RequestBody Produto produto) {
+		return produtoRepository.save(produto);
+	}
+	
 }
